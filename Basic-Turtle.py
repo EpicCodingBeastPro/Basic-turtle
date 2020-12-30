e229110187f73359f0b7f3c68ea0e336be609db0
EpicCodingBeastPro <76170598+EpicCodingBeastPro@users.noreply.github.com>
2020-12-30 12:05:39 +0530

    Add files via upload

---
+++ Basic-Turtle.py
@@ -0,0 +1,17 @@
+
+import turtle 
+
+t = turtle.Pen()#so here we are giving pen properties to t
+
+#so now let's just see how to make a square 
+
+def side():
+    t.forward(100)
+    t.left(90)
+
+side()
+side()
+side()
+side()
+
+turtle.done()
